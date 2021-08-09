6eb93d4eda5ca8438138eb3138763df10d487e62
LaneTrain47 <84758670+LaneTrain47@users.noreply.github.com>
2021-08-08 20:26:31 -0400

    20210808_202600

--- js/site.js
+++ js/site.js
@@ -1,27 +1,73 @@
-//CONTROLLER FUNCTION
+//Establishment of global variable definitions
+let startData = [3, 2, 4, 7, 6, 9];
+let userData = [];
+let sunsetVisible = [];
+let sunsetObscured = [];
 
-function getValues() {
+//Global function responsible for calculating values within array
+function calculate() {
+    let largest = 0;
+    sunsetVisible = [];
+    sunsetObscured = [];
+
+    if (userData.length == 0) {
+        for (let i = 0; i < startData.length; i++) {
+            if (startData[i] > largest) {
+                largest = startData[i];
+                sunsetVisible.push(startData[i]);
+            } else {
+                sunsetObscured.push(startData[i]);
+            }
+        }
+
+    } else {
+        for (let i = 0; i < userData.length; i++) {
+            if (userData[i] > largest) {
+                largest = userData[i];
+                sunsetVisible.push(userData[i]);
+            } else {
+                sunsetObscured.push(startData[i]);
+            }
+        }
+    }
 
     //Retrieve values from all 6 input fields
-    let building1Value = document.getElementById("building1").value;
-    let building2Value = document.getElementById("building2").value;
-    let building3Value = document.getElementById("building3").value;
-    let building4Value = document.getElementById("building4").value;
-    let building5Value = document.getElementById("building5").value;
-    let building6Value = document.getElementById("building6").value;
 
-    //Parse to ensure that the user's inputs are interpreted as numbers
-    building1Value = parseInt(building1Value)
-    building2Value = parseInt(building2Value)
-    building3Value = parseInt(building3Value)
-    building4Value = parseInt(building4Value)
-    building5Value = parseInt(building5Value)
-    building6Value = parseInt(building6Value)
-}
+    function getValues() {
 
-//place the numbers into an array
+        let building1Value = document.getElementById("building1").value;
+        let building2Value = document.getElementById("building2").value;
+        let building3Value = document.getElementById("building3").value;
+        let building4Value = document.getElementById("building4").value;
+        let building5Value = document.getElementById("building5").value;
+        let building6Value = document.getElementById("building6").value;
 
-//evaluate the values within the array to check for building height
+        //Parse to ensure that the user's inputs are limited to numbers
+        building1Value = parseInt(building1Value);
+        building2Value = parseInt(building2Value);
+        building3Value = parseInt(building3Value);
+        building4Value = parseInt(building4Value);
+        building5Value = parseInt(building5Value);
+        building6Value = parseInt(building6Value);
 
-//output the values of the taller buildings to a "can see" field,
-//and the values of the shorter buildings to a "cannot see" field
+        //Ensure that the user's inputs are interpreted as numbers, then alert 
+        //the user that only numbers are allowed if NaN characters were used 
+
+        if (Number.isInteger(building1Value) && Number.isInteger(building2Value) &&
+            Number.isInteger(building3Value) && Number.isInteger(building4Value) &&
+            Number.isInteger(building5Value) && Number.isInteger(building6Value)) {
+
+            //Call the sunset function and write the values to the screen
+            let buildingArray = Sunset(building1Value, building2Value, building3Value, building4Value, building5Value, building6Value)
+            displayData(buildingArray);
+        } else {
+            alert("Only numbers can be used.")
+        }
+    }
+
+    //place the numbers into an array
+
+    //evaluate the values within the array to check for building height
+
+    //output the values of the taller buildings to a "can see" field,
+    //and the values of the shorter buildings to a "cannot see" field
