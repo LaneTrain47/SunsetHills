137e4429819b13d30ed2b6de00aede2222acd4f0
LaneTrain47 <84758670+LaneTrain47@users.noreply.github.com>
2021-09-23 03:27:10 -0400

    20210921_195000
    Javascript WIP

--- js/site.js
+++ js/site.js
@@ -31,43 +31,60 @@ function calculate() {
         }
     }
 
-    //Retrieve values from all 6 input fields
+    displayOutput();
+}
+//Retrieve values from all 6 input fields
 
-    function getValues() {
+function getValues() {
 
-        let building1Value = document.getElementById("building1").value;
-        let building2Value = document.getElementById("building2").value;
-        let building3Value = document.getElementById("building3").value;
-        let building4Value = document.getElementById("building4").value;
-        let building5Value = document.getElementById("building5").value;
-        let building6Value = document.getElementById("building6").value;
+    let building1Value = document.getElementById("building1").value;
+    let building2Value = document.getElementById("building2").value;
+    let building3Value = document.getElementById("building3").value;
+    let building4Value = document.getElementById("building4").value;
+    let building5Value = document.getElementById("building5").value;
+    let building6Value = document.getElementById("building6").value;
 
-        //Parse to ensure that the user's inputs are limited to numbers
-        building1Value = parseInt(building1Value);
-        building2Value = parseInt(building2Value);
-        building3Value = parseInt(building3Value);
-        building4Value = parseInt(building4Value);
-        building5Value = parseInt(building5Value);
-        building6Value = parseInt(building6Value);
+    //Parse to ensure that the user's inputs are limited to numbers
+    building1Value = parseInt(building1Value);
+    building2Value = parseInt(building2Value);
+    building3Value = parseInt(building3Value);
+    building4Value = parseInt(building4Value);
+    building5Value = parseInt(building5Value);
+    building6Value = parseInt(building6Value);
 
-        //Ensure that the user's inputs are interpreted as numbers, then alert 
-        //the user that only numbers are allowed if NaN characters were used 
+    //Ensure that the user's inputs are interpreted as numbers, then alert 
+    //the user that only numbers are allowed if NaN characters were used 
 
-        if (Number.isInteger(building1Value) && Number.isInteger(building2Value) &&
-            Number.isInteger(building3Value) && Number.isInteger(building4Value) &&
-            Number.isInteger(building5Value) && Number.isInteger(building6Value)) {
+    if (Number.isInteger(building1Value) && Number.isInteger(building2Value) &&
+        Number.isInteger(building3Value) && Number.isInteger(building4Value) &&
+        Number.isInteger(building5Value) && Number.isInteger(building6Value)) {
 
-            //Call the sunset function and write the values to the screen
-            let buildingArray = Sunset(building1Value, building2Value, building3Value, building4Value, building5Value, building6Value)
-            displayData(buildingArray);
-        } else {
-            alert("Only numbers can be used.")
-        }
+        //Call the sunset function and write the values to the screen
+        userData = [building1Value, building2Value, building3Value, building4Value, building5Value, building6Value]
+        calculate();
+    } else {
+        alert("Only numbers can be used.")
+    }
+}
+
+function displayOutput() {
+
+    let light = document.getElementById("SunsetVisible");
+    let dark = document.getElementById("SunsetObscured");
+
+    for (let i = 0; i < sunsetVisible.length; i++) {
+        light.innerHTML += `<br>${sunsetVisible[i]}`
+    }
+    for (let i = 0; i < sunsetObscured.length; i++) {
+        dark.innerHTML += `<br>${sunsetObscured[i]}`
     }
 
-    //place the numbers into an array
 
-    //evaluate the values within the array to check for building height
 
-    //output the values of the taller buildings to a "can see" field,
-    //and the values of the shorter buildings to a "cannot see" field
+}
+
+
+
+
+//output the values of the taller buildings to a "can see" field,
+//and the values of the shorter buildings to a "cannot see" field
